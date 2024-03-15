60a05227d4fc2ce63d167ea64dca6759e60a49a2
mt <seek.mt0.sincenoe@gmail.com>
2024-03-16 03:02:26 +0900

    更新

--- src/php01/index06.php
+++ src/php01/index06.php
@@ -1,5 +1,12 @@
 <?php
-function gokaku($score1,$score2,$score3){$sum = $score1 + $score2 + $score3 ;
-if($sum>210){echo "合格点は".$sum."なので合格です";}else{echo "合格点は".$sum."なので不合格です";}}
-echo (gokaku(80,20,20));
+function gokaku($score1, $score2, $score3)
+{
+    $sum = $score1 + $score2 + $score3;
+    if ($sum > 210) {
+        echo "合格点は" . $sum . "なので合格です";
+    } else {
+        echo "合格点は" . $sum . "なので不合格です";
+    }
+}
+echo (gokaku(80, 20, 20));
 
