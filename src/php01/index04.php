b94111cf1378b48c5db4a69b06e5009811d94080
mt <seek.mt0.sincenoe@gmail.com>
2024-03-16 01:58:07 +0900

    "更新"

---
+++ src/php01/index04.php
@@ -0,0 +1,4 @@
+<?php
+$people="Jiro";
+$result=($people=="Taro")?"is Taro" : "is not Taro";
+echo $result;
---
+++ src/php01/index05.php
@@ -0,0 +1,21 @@
+<?php
+// for($i = 1;$i<=5;$i++){echo $i*2;echo "<br>";}
+// $a=1;
+// while($a<=5){echo $a*2; $a++;}
+// echo "<br>";
+
+// $b=1;
+// while($b<=20){echo $b."<br>";$b++;}
+
+
+// $count=0;
+
+// while($count<=100){if($count===20){break;}elseif($count%3==0){$count++;}else{echo $count;echo "<br>";$count++;}}
+
+// $nume=0;
+
+// do{echo "num=".$nume."<br>";$nume++;}while($nume<3);
+
+// for($i=1;$i<=50;$i++){if($i%3==0){if($i%5==0){echo "FizzBuzz"."<br>";}else{echo "Fizz"."<br>";}}elseif($i%5==0){echo "Buzz"."<br>";}else{echo $i."<br>";}};
+
+for($i=1;$i<=5;$i++){for($j=1;$j<=5;$j++){echo "●";}echo "<br>";}
