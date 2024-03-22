25b8f694fad4aa15eee9eb6bf134954b7eca7020
mt <seek.mt0.sincenoe@gmail.com>
2024-03-23 02:41:20 +0900

    add_file

--- src/php01/index02.php
+++ src/php01/index02.php
@@ -1,24 +1,24 @@
-<?php $sample = "Javascript" ;
-echo $sample ;
+<?php $sample = "Javascript";
+echo $sample;
 echo "<br/>";
-const Smp="Java";
+const Smp = "Java";
 echo Smp;
 echo "<br>";
 
-$name1="mao";
-$name2="tsuji";
-$num1=1229;
-$num2=902;
+$name1 = "mao";
+$name2 = "tsuji";
+$num1 = 1229;
+$num2 = 902;
 
-echo $name1.$name2;
+echo $name1 . $name2;
 echo "<br>";
-echo $num1.$num2;
+echo $num1 . $num2;
 echo "<br>";
 
-$name= "Tanaka";
-$last_name="Yamada";
-$first_name="Saburo";
+$name = "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
 
-echo $name."Jiro";
+echo $name . "Jiro";
 echo "<br>";
-echo $last_name.$first_name;
+echo $last_name . $first_name;
